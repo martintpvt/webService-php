1934a0b6faa208e1d9dbc174f7d63498a9646338
Martin <martin_almeida@outlook.com>
2015-03-01 13:47:40 -0500

    ruta

--- WebServiceSOAPSinWSDL/cliente.php
+++ WebServiceSOAPSinWSDL/cliente.php
@@ -1,7 +1,7 @@
 <?php
 
 	//Crear el cliente colocando la ruta URL donde se encuentra el servicio
-	$cliente = new SoapClient(null, array('location'=>'http://localhost/WebServiceSOAP/servicio.php',
+	$cliente = new SoapClient(null, array('location'=>'http://localhost/webService-php/WebServiceSOAPSinWSDL/servicio.php',
 		'uri'=>'urn:webservices', )); //Llamar al metodo como si fuera del cliente
 
 	echo $cliente->multiplicar(5, 4);
